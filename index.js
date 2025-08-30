a10fecbc5aa687eeab17c23ae32c9fceca242db8
3uomlkh <3uomlkh@gmail.com>
2025-08-30 16:15:14 +0900

    Add initial project files and GitHub workflow

---
+++ index.js
@@ -0,0 +1,53 @@
+import { writeFileSync } from 'node:fs';
+import Parser from "rss-parser";
+
+/**
+ * README.MD에 작성될 페이지 텍스트
+ * @type {string}
+ */
+let text = `# Hi there 👋
+
+## 🌱 I’m currently learning ...
+
+<p>
+  <img alt="" src="https://img.shields.io/badge/Java-007396?style=flat-square&logo=Java&logoColor=white"/>
+  <img alt="" src="https://img.shields.io/badge/Spring-6DB33F?style=flat-square&logo=Spring&logoColor=white"/>
+  <img alt="" src="https://img.shields.io/badge/Spring%20Boot-6DB33F?style=flat-square&logo=SpringBoot&logoColor=white"/>
+  <img alt="" src="https://img.shields.io/badge/AWS-232F3E?style=flat-square&logo=AmazonAWS&logoColor=white"/>
+  <img alt="" src="https://img.shields.io/badge/Docker-2496ED?style=flat-square&logo=Docker&logoColor=white"/>
+</p>
+
+## 📕 Latest Blog Posts
+
+`;
+
+// rss-parser 생성
+const parser = new Parser({
+    headers: {
+        Accept: 'application/rss+xml, application/xml, text/xml; q=0.1',
+    }});
+
+(async () => {
+
+    // 피드 목록
+    const feed = await parser.parseURL('https://3uomlkh.tistory.com/'); // 본인의 블로그 주소
+    
+    text += `<ul>`;
+    
+    // 최신 10개의 글의 제목과 링크를 가져온 후 text에 추가
+    for (let i = 0; i < 10; i++) {
+        const {title, link} = feed.items[i];
+        console.log(`${i + 1}번째 게시물`);
+        console.log(`추가될 제목: ${title}`);
+        console.log(`추가될 링크: ${link}`);
+        text += `<li><a href='${link}' target='_blank'>${title}</a></li>`;
+    }
+
+    text += `</ul>`;
+    
+    // README.md 파일 생성
+    writeFileSync('README.md', text, 'utf8', (e) => {
+        console.log(e);
+    })
+    console.log('업데이트 완료');
+})();
